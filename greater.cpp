b802762708eb2d11b26c1db072a5a9f19d3c6462
Dave Taylor <davehtaylor@me.com>
2016-10-14 23:06:22 -0400

    Another exercise

---
+++ greater.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int greater(int x, int y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+    else 
+    {
+        return y;
+    }
+}
+
+int main()
+{
+    int usernum1, usernum2;
+
+    std::cout << "Please enter a number: ";
+
+    std::cin >> usernum1;
+
+    std::cout << "Please enter another number: ";
+
+    std::cin >> usernum2;
+
+    std::cout << greater(usernum1, usernum2) 
+        << " is the larger of the two." << std::endl;
+
+    return 0;
+}
