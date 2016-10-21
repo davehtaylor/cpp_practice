8d740fa2bb221d165f7cf9a8c217e4e57d151f4f
Dave Taylor <davehtaylor@me.com>
2016-10-20 22:07:20 -0400

    Adding Magic Eight Ball game

---
+++ magicEightBall.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <chrono> //std::chrono::seconds
+#include <thread> //std::this_thread::sleep_for
+#include <random> //rand()
+
+void thinking()
+{
+    //Give some pause to appear to be thinking about the answer
+
+    int i = 1;
+
+    std::cout << "Contemplating ";
+
+    while (i <= 3)
+    {
+        std::cout << '.';
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        i++;
+    }
+}
+
+
+int main()
+{
+    // Variables to print the title banner
+    std::string stars(26, '*');
+    std::string spaces(5, ' ');
+
+    // Random index to choose from the responses array
+    srand(time(NULL));
+    int rand_index = rand() % 19;
+
+    // Eight ball responses
+    std::string responses[] = {"It is certain",
+                                "It is decidedly so",
+                                "Without a doubt",
+                                "Yes definitely",
+                                "You may rely on it",
+                                "As I see it, yes",
+                                "Most likely",
+                                "Outlook good",
+                                "Yes",
+                                "Signs point to yes",
+                                "Reply hazy try again",
+                                "Ask again later",
+                                "Better not tell you now",
+                                "Cannot predict now",
+                                "Concentrate and ask again",
+                                "Don't count on it",
+                                "My reply is no",
+                                "My sources say no",
+                                "Outlook not so good",
+                                "Very doubtful"};
+    
+    // Print a pretty banner
+    std::cout << std::endl;
+    std::cout << stars << std::endl;
+    std::cout << spaces;
+    std::cout << "MAGIC EIGHT BALL";
+    std::cout << spaces << std::endl;
+    std::cout << stars << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Think long and hard about your question. \n"
+        "When you are ready, press the enter key "
+        "to recieve your answer." << std::endl;
+
+    getchar();
+
+    thinking();
+    std::cout << std::endl;
+
+    std::cout << responses[rand_index] << std::endl;
+    std::cout << std::endl;
+
+    return 0;
+}
