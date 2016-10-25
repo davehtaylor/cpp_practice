7cda0a9e887e9d5bd8348b728f0b8496658cb60c
Dave Taylor <davehtaylor@me.com>
2016-10-25 15:11:38 -0400

    Created a function for the string splitting code. Still working out how I want to use that data. Also fixed a typo in the Eighties trivia file.

--- trivia.cpp
+++ trivia.cpp
@@ -11,6 +11,45 @@ struct Player {
 };
 
 
+// Function to split a string on a delimiter.
+//
+// Arguments taken: a string. Possilby a vector. Still
+// working on that bit.
+//
+// Returns: not sure yet. May use return, might simply
+// write to a vector. Using void return at the moment until 
+// I sort it out.
+void string_split(std::string s) {
+
+    char delim = '\"';
+    std::string comma = ",";
+    int start = 0;
+    int found = s.find(delim);
+
+    while (found != std::string::npos) {
+        
+        // Make easy variables for the substrings
+        std::string tok = s.substr(start, (found - start));
+        std::string tok_end = s.substr(start, s.length());
+
+        // If the substring is just a comma, don't print anything.
+        // Otherwise, print the substring. This will allow the string
+        // to be split at the quotes, and then drop the commas between
+        // Eventually this won't print things out, but will most likely
+        // write the tokens to something else. Will update as I change it.
+        if (tok == comma) {
+            std::cout << "";
+        } else {
+            std::cout << tok << std::endl;
+        }
+
+        start = (found + 1);
+        found = s.find(delim, start);
+
+    }
+}
+
+
 // Take the file needed for the questions, open it, add the info to a
 // vector, then close the file. 
 //
@@ -130,8 +169,6 @@ int main() {
     }
 
 
-    // If the player chooses to play, game begins here
-
     std::cout << "Please enter your name: ";
     std::cin >> Player1.name;
 
@@ -146,33 +183,7 @@ int main() {
         i++;
     }
 
-    // Figuring out how to split a string on a delimiter
-
-    std::string s = questions_and_answers[0];
-    char delim = '\"';
-    std::string comma = ",";
-    int start = 0;
-    int found = s.find(delim);
-
-    while (found != std::string::npos) {
-        
-        // Make easy variables for the substrings
-        std::string tok = s.substr(start, (found - start));
-        std::string tok_end = s.substr(start, s.length());
-
-        // If the substring is just a comma, don't print anything.
-        // Otherwise, print the substring. This will allow the string
-        // to be split at the quotes, and then drop the commas between
-        if (tok == comma) {
-            std::cout << "";
-        } else {
-            std::cout << tok << std::endl;
-        }
-
-        start = (found + 1);
-        found = s.find(delim, start);
-
-    }
+    string_split(questions_and_answers[0]);
 
     return 0;
 }
